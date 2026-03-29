5d159e4995f82d739d404c2886faef3bfb12aaeb
Aditya Srivastava <149177246+addysrii@users.noreply.github.com>
2026-03-29 17:30:01 +0530

    Update axios.js

--- AnnaSeva_Frontend/Frontend/src/api/axios.js
+++ AnnaSeva_Frontend/Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://annaseva-1.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
